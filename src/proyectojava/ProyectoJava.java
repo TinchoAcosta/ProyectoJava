29c818b08ac7ac595365b2ccccd85847e52e7c13
Débora Moyano <debo4606@gmail.com>
2024-03-20 11:02:00 -0300

    Agregue una clase

--- src/proyectojava/ProyectoJava.java
+++ src/proyectojava/ProyectoJava.java
@@ -15,6 +15,7 @@ public class ProyectoJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        clase nuevaclase = new nuevaclase();
     }
-    
+
 }
